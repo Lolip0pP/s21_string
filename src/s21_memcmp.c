692f5ecda9e51ea70942264af1cd547bc2c80666
katerina <bandwidl@student.21-scool.ru>
2024-05-26 18:59:51 +0300

    all tests

--- src/s21_memcmp.c
+++ src/s21_memcmp.c
@@ -2,7 +2,7 @@
 
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
   if (n != 0) {
-    const unsigned char *p1 = str1, *p2 = str2;
+    const char *p1 = str1, *p2 = str2;
 
     do {
       if (*p1++ != *p2++)
