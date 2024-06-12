6a789553733f28392d4f7f194d16016c5d8a8776
Dmitrii Nikoshin <d.nikoshin@gmail.com>
2024-06-12 15:43:43 +0300

    little change in s21_sscanf.h v2

--- src/s21_sscanf.c
+++ src/s21_sscanf.c
@@ -1,5 +1,46 @@
 #include "s21_string.h"
-#include "s21_sscanf.h"
+
+#define DEFAULT_WIDTH 50
+
+typedef struct {
+  s21_size_t pos_str;
+  s21_size_t pos_format;
+  int count;
+  int error;
+} pos_format_t;
+
+typedef struct {
+  int start;
+  int end;
+  int width;
+  int size;
+  int ignor;
+  char type;
+  char flags;
+  int precision;
+} type_label_t;
+
+void check_format(const char *str, const char *format, va_list parm, pos_format_t *pf);
+type_label_t get_signature(const char *format, pos_format_t *pf);
+void cmp_symbol(const char *str, const char *format, pos_format_t *pf);
+int get_size_type(const char *format, s21_size_t *position);
+int is_digital(char ch, int base);
+int is_flag(char ch);
+int is_size(char ch);
+int is_end_str(char ch);
+int is_spec(char ch);
+int char_to_number(char ch);
+int check_format_base(const char *str, s21_size_t *position);
+long long int str_to_int(const char *str, s21_size_t *position, int base, int width, int *error);
+long double str_to_float(const char *format, pos_format_t *pf, type_label_t type);
+void find_and_set_value(type_label_t type, const char *str, pos_format_t *pf, va_list parm);
+void make_di(void *label, const char *str, pos_format_t *pf, type_label_t type);
+void make_c(void *label, const char *str, pos_format_t *pf, type_label_t type);
+void make_s(void *label, const char *str, pos_format_t *pf, type_label_t type);
+void make_fGgEe(void *label, const char *str, pos_format_t *pf, type_label_t type);
+void make_n(void *label, pos_format_t *pf, type_label_t type);
+void make_uoxX(void *label, const char *str, pos_format_t *pf, type_label_t type);
+void make_p(void **label, const char *str, pos_format_t *pf, type_label_t type);
 
 int s21_sscanf(const char *str, const char *format, ...) {
   s21_size_t len_format = s21_strlen(format);
